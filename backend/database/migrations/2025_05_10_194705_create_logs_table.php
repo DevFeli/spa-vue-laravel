2e36be9bc3c5d60b02c106ac33316a4b48590de2
Felipe <freis@nashai.ai>
2025-05-10 16:54:20 -0300

    feature/create-mcr

---
+++ backend/database/migrations/2025_05_10_194705_create_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->nullable();
+            $table->integer('product_id')->nullable();
+            $table->string('action'); //enum
+            $table->string('route');
+            $table->text('error')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+};
